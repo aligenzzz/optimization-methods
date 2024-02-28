720caebe14f98294cffea985a2d90bb426e26a9d
aligenzzz <alina_bybko@mail.ru>
2024-02-28 23:21:19 +0300

    fix lab2

--- lab2/simplex_method.py
+++ lab2/simplex_method.py
@@ -1,10 +1,53 @@
 import numpy as np
 
+
+# from lab 1
+def matrix_with_modified_column_inversion(A: np.ndarray, A_inverted: np.ndarray, 
+                                          x: np.ndarray, i: int) -> np.ndarray:
+    n = A.shape[0]
+    if i < 0 or i > n:
+        raise Exception('Invalid index!')
+    i -= 1
+
+    # Step 1
+    l = A_inverted.dot(x)
+    if l[i] == 0:
+        raise Exception('Modified matrix can\'t be inverted!')
+    
+    # Step 2
+    l_copied = np.copy(l)
+    l_copied[i] = -1
+
+    # Step 3 
+    l_new = (-1 / l[i]) * l_copied
+
+    # Step 4
+    Q = np.eye(n)
+    Q[:, i] = l_new 
+
+    # Step 5
+    A_result = np.zeros((n, n))
+    for t in range(n):
+        for j in range(n):
+            if t != i:
+                A_result[t][j] = Q[t][t] * A_inverted[t][j] + Q[t][i] * A_inverted[i][j]
+            else:
+                A_result[t][j] = Q[t][t] * A_inverted[t][j]
+
+    return A_result
+
+
 def second_phase(A: np.ndarray, c_T: np.ndarray, x_T: np.ndarray, B: list) -> np.ndarray:
+    was = False
+
     while True:
         # Step 1
         A_B = A[:, np.array(B) - 1]
-        A_B_inverted = np.linalg.inv(A_B)
+
+        if was:
+            A_B_inverted = matrix_with_modified_column_inversion(A_B, A_B_inverted, A[:, j0], k + 1)
+        else:
+            A_B_inverted = np.linalg.inv(A_B)
 
         # Step 2
         c_B_T = c_T[np.array(B) - 1]
@@ -50,4 +93,6 @@ def second_phase(A: np.ndarray, c_T: np.ndarray, x_T: np.ndarray, B: list) -> np
             if j != j0 + 1:
                 x_T[j - 1] = x_T[j - 1] - theta0 * z[i]
         x_T[j_k - 1] = 0
+
+        was = True
         
